f810773d42283b1a3c03be469bfcb2df70133421
jiangcong <jiangcong@nowcoder.com>
2022-03-10 11:37:35 +0800

    'Test1'

---
+++ src/main/java/com/nowcoder/mavenmodel1/App.java
@@ -0,0 +1,13 @@
+package com.nowcoder.mavenmodel1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
